1d1bbbc731986a8c97311fb0d1b83a14a7ed3aa4
shreenath92 <shreenathhebbar@gmail.com>
2025-11-07 10:59:43 +0530

    docker Integrartion

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -1,5 +1,12 @@
 import os
 import sys
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/shreenath6362/network_security-MLOPs.mlflow/"
+os.environ["MLFLOW_TRACKING_USERNAME"]="shreenath6362"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="fe6c4fee1f44a488e14cc148d1ef40645d0bf63d"
+os.environ["MLFLOW_ENABLE_SYSTEM_METRICS_LOGGING"] = "false"
+os.environ["MLFLOW_LOG_MODEL_SIGNATURES"] = "false"
+os.environ["MLFLOW_FLATTEN_PARAMS"] = "false"
+
 
 from networksecurity.exception.exception import NetworkSecurityException 
 from networksecurity.logging.logger import logging
@@ -27,14 +34,7 @@ import mlflow
 from urllib.parse import urlparse
 
 import dagshub
-#dagshub.init(repo_owner='krishnaik06', repo_name='networksecurity', mlflow=True)
-
-os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/krishnaik06/networksecurity.mlflow"
-os.environ["MLFLOW_TRACKING_USERNAME"]="krishnaik06"
-os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ece21e0e2adb4e36a250ae3251f"
-
-
-
+dagshub.init(repo_owner='shreenath6362', repo_name='network_security-MLOPs', mlflow=True)
 
 
 class ModelTrainer:
@@ -45,31 +45,23 @@ class ModelTrainer:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def track_mlflow(self,best_model,classificationmetric):
-        mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+    def track_mlflow(self, best_model, classificationmetric):
+        import mlflow
+        from urllib.parse import urlparse
+
+        mlflow.set_tracking_uri("https://dagshub.com/shreenath6362/network_security-MLOPs.mlflow")
+        
+
         with mlflow.start_run():
-            f1_score=classificationmetric.f1_score
-            precision_score=classificationmetric.precision_score
-            recall_score=classificationmetric.recall_score
+            mlflow.log_metric("f1_score", classificationmetric.f1_score)
+            mlflow.log_metric("precision", classificationmetric.precision_score)
+            mlflow.log_metric("recall", classificationmetric.recall_score)
 
-            
+           
+    
 
-            mlflow.log_metric("f1_score",f1_score)
-            mlflow.log_metric("precision",precision_score)
-            mlflow.log_metric("recall_score",recall_score)
-            mlflow.sklearn.log_model(best_model,"model")
-            # Model registry does not work with file store
-            if tracking_url_type_store != "file":
-
-                # Register the model
-                # There are other ways to use the Model Registry, which depends on the use case,
-                # please refer to the doc for more information:
-                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.sklearn.log_model(best_model, "model", registered_model_name=best_model)
-            else:
-                mlflow.sklearn.log_model(best_model, "model")
-
+            #
+           
 
         
     def train_model(self,X_train,y_train,x_test,y_test):
